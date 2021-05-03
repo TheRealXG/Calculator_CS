9451de907bab06d72c9880f1e841c7dfbeac693d
James Brewer <james.brewer.15@us.af.mil>
2021-05-03 12:32:31 -0500

    Added containers to dockerhub and updated script to pull containers

--- desktop_sim.sh
+++ desktop_sim.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 # Setup Docker User Defined Bridge or continue if it already exists
-docker network create --driver=bridge --subnet=10.18.0.0/16 --gateway=10.18.0.1 calc-bridge || true
+bridge_name=calc-bridge
+docker network create --driver=bridge --subnet=10.18.0.0/16 --gateway=10.18.0.1 $bridge_name || true
 
 # Start A661 Server container
 xhost +local:docker
-docker run -ti -e DISPLAY --network=calc-bridge --hostname=server --name=server_con -v ~/Documents/calculator:/volume -v /tmp/.X11-unix:/tmp/.X11-unix -d a661_server
+server_name=server_con
+docker run -ti -e DISPLAY --network=$bridge_name --hostname=server --name=$server_name -v ~/Documents/calculator:/volume -v /tmp/.X11-unix:/tmp/.X11-unix -d therealxg/azure_pipeline:calc_a661_server
 
-sleep 5
+sleep 3
 a661_pid=`pgrep A661S`
 
 # Start Calc UA Docker Container (will need to update container location to registry eventually)
-docker run -ti --cap-add=NET_ADMIN --device=/dev/net/tun --network=calc-bridge --hostname=calc_ua --name=calc_ua_con -v ~/Documents/calculator:/volume -d ua_qemu
+ua_name=calc_ua_con
+docker run -ti --cap-add=NET_ADMIN --device=/dev/net/tun --network=$bridge_name --hostname=calc_ua --name=$ua_name -v "$(pwd)":/volume -d therealxg/azure_pipeline:calc_ua
 
 # What for the Server GUI to be closed (which will stop that process ID)
 tail --pid=$a661_pid -f /dev/null
 # Cleanup docker containers after the server is quit
-docker kill calc_ua_con
-docker rm calc_ua_con
-docker rm server_con
+docker kill $ua_name
+docker rm $ua_name
+docker rm $server_name
